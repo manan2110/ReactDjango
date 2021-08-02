318a9c516e7e6884f790368232c9e59661e07d94
manan2110 <manan.co.in@gmail.com>
2021-08-02 23:57:09 +0530

    init

---
+++ frontend/gui/src/store/actions/actionTypes.js
@@ -0,0 +1,5 @@
+// Things that can happen are stored in the store, actions realted to authentication
+export const AUTH_START = 'AUTH_START';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAIL = 'AUTH_FAIL';
+export const AUTH_LOGOUT = 'AUTH_LOGOUT';
